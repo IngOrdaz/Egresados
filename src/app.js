ccfd23109ea50ab9265a0a5b2b3b83e05f097ca8
IngOrdaz <91166458+IngOrdaz@users.noreply.github.com>
2022-04-28 15:39:25 -0500

    inicial
    Configuración inicial del proyecto

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import indexRoutes from "./routes/index.routes";
+//import exphbs from 'express-handlebars'; es el de abajo pero ya no da error el de abajo
+const exphbs = require("express-handlebars");
+import path from "path";
+
+//init
+const app = express();
+
+//settings
+app.set("views", path.join(__dirname, "views"));
+app.engine(
+  ".hbs",
+  exphbs.engine({
+    extname: ".hbs",
+    defaultLayout: "main",
+    layoutsDir: path.join(app.get("views"), "layouts"),
+  })
+);
+app.set("view engine", ".hbs");
+
+//Routes
+app.use(indexRoutes);
+
+export default app;
