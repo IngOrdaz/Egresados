bce2e211f9623be3a3b60d187aa54202d6fe8f40
IngOrdaz <91166458+IngOrdaz@users.noreply.github.com>
2022-05-17 23:08:13 -0500

    front files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import app from "./app";
 import './config';
 import './database';
-
+//ejemplo de branch
 app.listen(process.env.PORT);
 console.log("Server on port", 3001);
 //Ignacio Ordaz
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -10,12 +10,30 @@ router.get("/about", (req, res) => {
   res.render("about");
 });
 
-router.get("/users/register", (req, res) => {
-  res.render("usersRegister");
+router.get("/users/registerEgresados", (req, res) => {
+  res.render("usersRegisterEgresados");
+});
+
+router.get("/users/registerAlumnos", (req, res) => {
+  res.render("usersRegisterAlumnos");
 });
 
 router.get("/login", (req, res) => {
   res.render("login");
 });
+
+router.get("/muro", (req, res) => {
+  res.render("muro");
+});
+
+router.get("/users/modifyEgresados", (req, res) => {
+  res.render("usersModifyEgresados");
+});
+
+router.get("/users/profile", (req, res) => {
+  res.render("usersProfile");
+});
+
+
 
 export default router;
